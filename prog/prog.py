3c190a985d39f8d28449cf7e580b8ed785a39881
B4rkyB4rky <vayssielouis1@gmail.com>
2020-12-01 00:16:16 +0100

    commit prog

--- prog/prog.py
+++ prog/prog.py
@@ -1,5 +1,7 @@
 def valeur(char):
     val = 0
+    if char == '':
+        val = 0
     if char == 'I':
         val += 1
     if char == 'V':
@@ -17,15 +19,35 @@ def valeur(char):
 
     return val
 
+
 def valeur_totale(char):
     total = 0
-    for i in range(0, len(char)):
-        if i != len(char)-1:
-            if valeur(char[i + 1]) > valeur(char[i]):
-                total += (valeur(char[i+1]) - valeur(char[i]))
+    cpt = 0
+    l = len(char)
+    while cpt < l:
+        if cpt < l - 1:
+            if valeur(char[cpt+1]) > valeur(char[cpt]):
+                total += valeur(char[cpt+1]) - valeur(char[cpt])
+                cpt = cpt + 2
+            else:
+                total += valeur(char[cpt])
+                cpt += 1
 
-
-        total += valeur(char[i])
-
+        else:
+            total += valeur(char[cpt])
+            cpt += 1
 
     return total
+
+def calculatrice(operateur,char1,char2):
+    if operateur == '-':
+        return valeur_totale(char1) - valeur_totale(char2)
+    if operateur == '+':
+        return valeur_totale(char1) + valeur_totale(char2)
+    if operateur == '*':
+        return valeur_totale(char1) * valeur_totale(char2)
+    if operateur == '/':
+        if valeur_totale(char2) == 0:
+            return "erreur"
+        else:
+            return valeur_totale(char1) / valeur_totale(char2)
